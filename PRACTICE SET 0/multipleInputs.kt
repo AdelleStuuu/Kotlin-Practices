d820ec817a3a31e4f45f8d0789b193d5dfc6ce40
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-22 16:43:18 +0800

    checking how kotlin handles  multiple inputs

---
+++ PRACTICE SET 0/multipleInputs.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("\nHello! Enter your name here! ");
+    var name = readln();
+    println("Nice! Enter your age here! ");
+    var age = readln();
+
+    println("\nYour name is $name and you are $age years old!\n");
+}
