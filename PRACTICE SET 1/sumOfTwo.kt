b546e14683fae28e058196ea57e60dc2b7016ff6
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-23 18:28:20 +0800

    adds two numbers and shows the sum

---
+++ PRACTICE SET 1/sumOfTwo.kt
@@ -0,0 +1,11 @@
+fun main() {
+    print("\nEnter a number: ");
+    var num1 = readln(); 
+
+    print("Enter a number: ");
+    var num2 = readln();
+
+    var sum = num1.toInt() + num2.toInt();
+
+    print("the sum of $num1 and $num2 is $sum!\n\n");
+}
